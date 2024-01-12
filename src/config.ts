7ccc9eefe40a038f2fd1a9be7ed58d6b224a4950
Vern Zhang <vern@manta.network>
2024-01-12 15:46:28 +0800

    chore: update archive source url

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const config: ProcessorConfig = {
   chainName: 'manta',
   prefix: 'manta',
   dataSource: {
-    archive: 'https://dev.manta-debug.graphql.nonprod-mantadex.com/graphql',
+    archive: 'https://prod.manta-debug.graphql.mantadex.com/graphql',
     chain: CHAIN_NODE,
   },
 }
