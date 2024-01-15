7b93e03c9b4a81f262644fb8fbb2815813f77ab9
Vern Zhang <vern@manta.network>
2024-01-15 16:05:01 +0800

    chore: update pricing token

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -11,8 +11,8 @@ export const WNATIVE = '2104-0-0' // MANTA
 export const DOT = '2104-2-8'  // DOT
 export const USDT = '2104-2-9' // USDT
 export const GLMR = '2104-2-10' // Moonbeam
-export const ACA = '2104-2-11' // Acala
-export const LDOT = '2104-2-12' // LDOT
+// export const ACA = '2104-2-11' // Acala
+export const LDOT = '2104-2-15' // LDOT
 export const MANDEX = '2104-2-31' // MANDEX
 // export const WNATIVE_USDC = '2104-2-8796093023744'
 
@@ -23,7 +23,7 @@ export const WHITELIST: string[] = [
   DOT, // DOT
   USDT,
   GLMR,
-  ACA,
+  // ACA,
   LDOT,
   MANDEX
 ]
