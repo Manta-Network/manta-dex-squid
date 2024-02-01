f8c2b42f2df06c132af1be767cf79a65449832f7
Vern Zhang <vern@manta.network>
2024-02-01 14:14:34 +0800

    chore: add usdt currency

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,7 @@ export const CHAIN_NODE = 'wss://c1.manta.seabird.systems'
 export const TOKEN_METADATA_MAP: { [address: string]: TokenBase } = {
   '2104-0-0': { name: 'Manta', symbol: 'MANTA', decimals: 18 },
   '2104-2-8': { name: 'Polkadot', symbol: 'DOT', decimals: 10 },
+  '2104-2-9': { name: 'Tether USD', symbol: 'USDT', decimals: 6 },
   '2104-2-10': { name: 'Moonbeam', symbol: 'GLMR', decimals: 18 },
   '2104-2-15': { name: 'Lido (via Acala Wormhole)', symbol: 'LDO', decimals: 18 },
   // '2104-2-31': { name: 'DAI Stablecoin (MRL)', symbol: 'DAI.MRL.ETH', decimals: 18 },
@@ -13,7 +14,6 @@ export const TOKEN_METADATA_MAP: { [address: string]: TokenBase } = {
   // '2104-2-1028': { name: 'vsKSM', symbol: 'vsKSM', decimals: 12 },
   // '2104-2-521': { name: 'RMRK', symbol: 'RMRK', decimals: 10 },
   // '2104-2-260': { name: 'vKusama', symbol: 'vKSM', decimals: 12 },
-  // '2104-2-2048': { name: 'USDT', symbol: 'USDT', decimals: 6 },
 }
 
 export const CHAIN_ID = 2104
